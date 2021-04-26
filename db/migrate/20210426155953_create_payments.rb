0ae84154130709ac21b6835a1013bab19d77eee3
tadeuasarro <tadeutas@gmail.com>
2021-04-26 13:00:20 -0300

    Generate the Payment model

---
+++ db/migrate/20210426155953_create_payments.rb
@@ -0,0 +1,10 @@
+class CreatePayments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :payments do |t|
+      t.string :type
+      t.float :value
+
+      t.timestamps
+    end
+  end
+end
