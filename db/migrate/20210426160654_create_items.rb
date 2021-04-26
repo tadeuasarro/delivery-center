05d6bdc10d4183ba8deb779f495669b17e29cf7e
tadeuasarro <tadeutas@gmail.com>
2021-04-26 13:07:39 -0300

    Migrate the DB

--- db/migrate/20210426160654_create_items.rb
+++ db/migrate/20210426160654_create_items.rb
@@ -6,7 +6,6 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.float :price
       t.integer :quantity
       t.float :total
-      t.array :subItems
 
       t.timestamps
     end
---
+++ db/schema.rb
@@ -0,0 +1,67 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_26_160654) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "externalCode"
+    t.string "name"
+    t.string "email"
+    t.string "contact"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "externalCode"
+    t.string "name"
+    t.float "price"
+    t.integer "quantity"
+    t.float "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "externalCode"
+    t.integer "storeId"
+    t.string "subTotal"
+    t.string "deliveryFee"
+    t.float "total_shipping"
+    t.string "total"
+    t.string "countrystate"
+    t.string "city"
+    t.string "district"
+    t.string "street"
+    t.string "complement"
+    t.float "latitude"
+    t.float "longitute"
+    t.string "dtOrderCreated"
+    t.string "postalCode"
+    t.string "number"
+    t.integer "customerId"
+    t.integer "paymentId"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.string "type"
+    t.float "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
